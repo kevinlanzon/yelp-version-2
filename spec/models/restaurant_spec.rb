58a4b484ccad3e8a6f6caa2ddc15cc511abf044c
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-01 13:20:22 +0100

    restaurant name has to be unique

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,4 +5,10 @@ describe Restaurant, :type => :model do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's")
+    restaurant = Restaurant.new(name: "Moe's")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
