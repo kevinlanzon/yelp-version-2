8045841e00ef1f2412cd5f07440720b57de37e86
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-12 13:16:37 +0100

    added association tests

--- Gemfile
+++ Gemfile
@@ -43,10 +43,10 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'spring', '~> 1.3.3'
   gem 'capybara'
   gem 'rspec-rails', '~> 3.2.1'
   gem 'rspec-collection_matchers'
-  gem 'shoulda-matchers'
+  gem 'shoulda'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402091909) do
+ActiveRecord::Schema.define(version: 20150412121552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150402091909) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "rating"
+    t.integer  "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -30,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150402091909) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "restaurant_id"
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
