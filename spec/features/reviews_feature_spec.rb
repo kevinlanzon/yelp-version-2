7aaecf8f943143ca161e2fc047f682cf2458d346
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-12 19:50:26 +0100

    test for leaving one review per restaurant failing

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -11,7 +11,6 @@ feature 'reviewing' do
     Restaurant.create name: 'KFC'
   end
 
-
   scenario 'allow users to fill in a review using a form' do
     visit '/restaurants'
     click_link 'Review KFC'
@@ -20,5 +19,16 @@ feature 'reviewing' do
     click_button 'Leave Review'
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content("this isn't chicken!")
+  end
+
+  scenario 'users can only leave one review per restaurant' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "this isn't chicken!"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    expect(current_path).to eq '/restaurants'
+    click_link 'Review KFC'
+    expect(page).to have_content("You have already reviewed this restaurant")
   end
 end
